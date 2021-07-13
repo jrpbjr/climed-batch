bfda6e32d39dd2f9f7ac2f7c68943e8bb0de190a
jrpbjr <jrpbjr@gmail.com>
2021-07-12 21:12:45 -0300

    inicio do projeto

---
+++ src/main/java/com/clinica/climed/ClimedApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.climed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimedApplication.class, args);
+	}
+
+}
