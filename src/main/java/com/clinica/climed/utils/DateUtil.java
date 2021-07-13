6465abcea0221477f74a047d350888e8caa78c2a
jrpbjr <jrpbjr@gmail.com>
2021-07-12 23:41:41 -0300

    utilitario de data para registro

---
+++ src/main/java/com/clinica/climed/utils/DateUtil.java
@@ -0,0 +1,20 @@
+package com.clinica.climed.utils;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class DateUtil {
+
+	public static String getNow() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public static Date toDate(LocalDateTime localDateTime){
+        Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
+        Date date = Date.from(instant);
+        return date;
+    }
+}
