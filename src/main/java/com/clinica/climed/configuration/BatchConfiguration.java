be015b60126afa8b256fc73ff3970a489baf03ca
jrpbjr <jrpbjr@gmail.com>
2021-07-13 23:27:35 -0300

    Bean para declara o Chuncklet-step etapas reader- lê registro| processor- processa registro | writer Escreve o registro no banco de dados e depois escrever no arquivo de  .csv de saída de sucesso

--- src/main/java/com/clinica/climed/configuration/BatchConfiguration.java
+++ src/main/java/com/clinica/climed/configuration/BatchConfiguration.java
@@ -1,5 +1,6 @@
 package com.clinica.climed.configuration;
 
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -32,6 +33,35 @@ public class BatchConfiguration {
 				.tasklet(new DoctorValidateTasklet("doctor-import"))
 				.build();
 	}
+	
+	@Bean
+	public Step doctorEnriquecimentoChunckletStep(ItemReader<DoctorDTO> doctorItemReader,
+			ItemProcessor<DoctorDTO, Doctor> doctorItemProcessor,
+			ItemWriter<Doctor> doctorItemWriter) {
+		
+		return stepBuilderFactory
+				.get("doctorEnriquecimentoChunckletStep")
+				.<DoctorDTO, Doctor>chunck(5)
+				.reader(doctorItemReader)
+				.processor(carroItemProcessor)
+				.writer(doctorItemWriter)
+				.build();
+	}
+	
+	@Bean
+	ItemReader<DoctorDTO> doctorItemReader(){
+		return new DoctorItemReader();
+	}
+	
+	@Bean
+	public ItemProcessor<DoctorDTO, Doctor> doctorItemProcessor(){
+		return new CarroItemProcessor();
+	}
+	
+	@Bean ItemWriter<Doctor> doctorItemWriter(){
+		return new DoctorItemWriter();
+	}
+	
 	
 	
 	
