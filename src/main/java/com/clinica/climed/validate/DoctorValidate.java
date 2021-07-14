0d7f01d4c97abeee97077aeb23ce0f45c18d8366
jrpbjr <jrpbjr@gmail.com>
2021-07-14 00:20:44 -0300

    validação registro

---
+++ src/main/java/com/clinica/climed/validate/DoctorValidate.java
@@ -0,0 +1,83 @@
+package com.clinica.climed.validate;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.clinica.climed.dto.DoctorDTO;
+import com.clinica.climed.utils.CsvFileUtilDoctor;
+import com.clinica.climed.converter.DoctorConverter;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DoctorValidate {
+	
+	 public DoctorValidate() {}
+	 
+	 public static List<DoctorDTO> validate(List<DoctorDTO> carroDtoList) throws IOException {
+
+	        List<DoctorDTO> validList = new ArrayList<>();
+	        List<DoctorDTO> invalidList = new ArrayList<>();
+
+	        carroDtoList.stream().forEach(dto -> {
+
+	            if(isValid(dto)){
+	                validList.add(dto);
+	            }else{
+	                invalidList.add(dto);
+	            }
+	        });
+
+	        createCsvToInvalidList(invalidList);
+	        return validList;
+	    }
+	 
+	 
+	 private static void createCsvToInvalidList(List<DoctorDTO> invalidList) throws IOException {
+
+	        if(!invalidList.isEmpty()){
+
+	            CsvFileUtilDoctor csvOutInvalid = new CsvFileUtilDoctor("invalid-import-doctor", false);
+
+	            invalidList.stream().forEach(doctor -> {
+	                try {
+	                    csvOutInvalid.writerError(DoctorConverter.doctorToStringArray(doctor));
+	                } catch (IOException e) {
+	                    e.printStackTrace();
+	                }
+	            });
+
+	            csvOutInvalid.closeWriter();
+	        }
+	    }
+	 
+	 
+	 public void emptyFile(LocalDateTime localDateTime) throws IOException {
+
+	        CsvFileUtilDoctor emptyCSV = new CsvFileUtilDoctor("empty-file",false);
+
+	        try {
+	            emptyCSV.writerError(DoctorConverter.getErrorToStringArray("Tentativa de Import de arquivo vazio!"));
+	            emptyCSV.closeWriter();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	 	 
+	 
+	 private static boolean isValid(DoctorDTO doctorDto) {
+		 
+				 
+	        return StringUtils.isNotBlank(doctorDto.getName());
+//	                && StringUtils.isNotBlank(doctorDto.getXXX())
+//	                && StringUtils.isNotBlank(doctorDto.getXXX())
+//	                && StringUtils.isNotBlank(doctorDto.getXXX())
+//	                && StringUtils.isNotBlank(doctorDto.getXXX())
+//	                && StringUtils.isNotBlank(doctorDto.getXXX()
+//	                		);
+	        
+	    }
+
+}
