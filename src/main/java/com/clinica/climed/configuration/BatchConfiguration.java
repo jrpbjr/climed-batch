625b3bbf1bd4463b515a882edb94d98f1a9100d7
jrpbjr <jrpbjr@gmail.com>
2021-07-13 21:28:05 -0300

    classe de configuração para declarar job, Tasklet -Validação, Chunckelet - leitura, processamento e escrita

---
+++ src/main/java/com/clinica/climed/configuration/BatchConfiguration.java
@@ -0,0 +1,28 @@
+package com.clinica.climed.configuration;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Jop job() {
+		return jobBuilderFactory.get("doctorJob").start(doctorValidateTaskletStep())
+				.next(doctorEnriquecimentoChunckletStep(doctorItemReader(), doctorItemProcessor(), doctorItemWritem()))
+				.build();
+
+	}
+
+}
